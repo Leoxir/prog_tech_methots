27bc2bd90eb003a560e645fa886fea521ae83954
Ilya Shishkin <i.shishkin20072703@gmail.com>
2025-10-08 16:05:13 +0300

    Delete prog_tech_methots directory

--- prog_tech_methots/server/sha384.cpp
+++
@@ -1,7 +0,0 @@
-#include "sha384.h"
-#include <QCryptographicHash>
-
-QString SHA384::hash(const QString& text) {
-    QByteArray ba = QCryptographicHash::hash(text.toUtf8(), QCryptographicHash::Sha384);
-    return ba.toHex();
-}
